bae26ed1426bb6018af7025d78ed9faa974e7b81
connor@connorontheweb.com <connor@connorontheweb.com>
2024-12-26 15:00:54 -0800

    readme

--- src/components/theme-select.jsx
+++ src/components/theme-select.jsx
@@ -4,6 +4,7 @@
 import { useTheme } from 'next-themes'
 
 export function ThemeSelect() {
+  // setTheme() from useTheme() from next-themes can be implemnted in any function triggered by any element or greater function. 'light' and 'dark' and 'system' are defaults, you can also create custom themes using next-themes and tailwind
   const { theme, setTheme } = useTheme();
 
   return (
